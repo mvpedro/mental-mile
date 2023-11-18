6719678193ab435c5ef6659fb05c487d931da24c
Pedro Vieira <mvpedro.cc@gmail.com>
2023-11-18 18:01:32 -0300

    chore: testing commit

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   disable:
-    process.env.NODE_ENV === 'development' ||
-    process.env.NODE_ENV === 'preview' ||
-    process.env.NODE_ENV === 'production',
+    process.env.NODE_ENV === 'development' 
+    // ||
+    // process.env.NODE_ENV === 'preview' ||
+    // process.env.NODE_ENV === 'production',
   // delete two lines above to enable PWA in production deployment
   // add your own icons to public/manifest.json
   // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
